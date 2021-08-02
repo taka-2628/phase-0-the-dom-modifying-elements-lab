67934398e57e2031c1e23d429891d1a9616b420d
Takashi <th2826@columbia.edu>
2021-08-02 12:34:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+//remove <main id="main"></main>
+main.remove();
+
+//add new header <h1>
+const newHeader = document.createElement ('h1');
+document.body.appendChild(newHeader);
+//add an id 'victory' to <hi>
+newHeader.setAttribute("id", "victory");
+//add "YOUR-NAME is the champion" to <h1>
+newHeader.innerHTML = "YOUR-NAME is the champion";
